c848e9e25756050297a46e63b61dfe32979462e4
victorliu <victor.liu@gmail.com>
2013-01-19 01:37:47 -0800

    Made debugging assertions present unless NDEBUG is defined, as is the typical convention.

--- RNP/Debug.hpp
+++ RNP/Debug.hpp
@@ -1,9 +1,9 @@
 #ifndef RNP_DEBUG_HPP_INCLUDED
 #define RNP_DEBUG_HPP_INCLUDED
 
-#ifdef RNP_ENABLE_DEBUG
+#ifndef NDEBUG
+
 #include <iostream>
-#endif
 
 #define RNPAssertStringify(s) #s
 #define RNPAssert(COND) RNP::Assert(COND, __FILE__, __LINE__, RNPAssertStringify(COND))
@@ -11,12 +11,16 @@
 namespace RNP{
 
 inline void Assert(bool cond, const char *file, int line, const char *str){
-#ifdef RNP_ENABLE_DEBUG
 	if(!cond){
 		std::cerr << "Assertion failed in " << file << ":" << line << ": " << str << std::endl;
 	}
-#endif
 }
+
+#else
+
+#define RNPAssert(COND)
+
+#endif
 
 }; // namespace RNP
 
